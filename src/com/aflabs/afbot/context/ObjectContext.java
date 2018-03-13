1d50f65d42080f047fa2e34c8b54191b8f1aecf4
shastrijs <shastri.jagarlapudi@gmail.com>
2018-03-13 16:32:32 -0400

    Test

--- src/com/aflabs/afbot/context/ObjectContext.java
+++ src/com/aflabs/afbot/context/ObjectContext.java
@@ -6,19 +6,6 @@ import java.util.List;
 
 public interface ObjectContext extends Context{
 	
-	List<Object> instincts = new ArrayList<Object>();	
-	List<Object> beliefs = new ArrayList<Object>();
-	List<Object> assumptions = new ArrayList<Object>();
-	List<Object> knowledge = new ArrayList<Object>();
-	List<Object> facts = new ArrayList<Object>();
-	List<Object> patterns = new ArrayList<Object>();
-	List<Object> stereotypes = new ArrayList<Object>();
-	
-	public void loadInstincts();
-	public void loadBeliefs();
-	public void loadAssumptions();
-	public void loadKnowledge();
-	public void loadFacts();	
-	public void loadPatterns();
+
 
 }
